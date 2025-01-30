435af43aeba4762c497d809c5495d22e7bfa92e1
steven03157 <127449263+steven03157@users.noreply.github.com>
2025-01-30 15:22:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,3 +59,8 @@ def draw():
 
 if __name__ == '__main__':
     app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # 取得 Render 指定的 PORT，預設 5000
+    app.run(host='0.0.0.0', port=port)
